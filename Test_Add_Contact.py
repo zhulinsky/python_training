24c37075b288e294ed89a007b123b675ee84af30
ezhulinsky <mous_dom@mail.ru>
2019-04-26 18:24:32 +0300

    создан тест в рекордере, перенесен в проект, запуск ок_1(убраны лишние строки/ переименована константа driver/разбито на блоки + коментарии)

--- Test_Add_Contact.py
+++ Test_Add_Contact.py
@@ -24,39 +24,51 @@ class TestAddContact(unittest.TestCase):
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys("secret")
         wd.find_element_by_xpath("//input[@value='Login']").click()
-        # forma
+        # neu form
         wd.find_element_by_link_text("add new").click()
+        # fitstname
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
         wd.find_element_by_name("firstname").send_keys("jdfdsfgs")
+        # middlname
         wd.find_element_by_name("middlename").click()
         wd.find_element_by_name("middlename").clear()
         wd.find_element_by_name("middlename").send_keys("gdhf")
+        # lastname
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
         wd.find_element_by_name("lastname").send_keys("kldfgj")
+        # address
         wd.find_element_by_name("address").click()
         wd.find_element_by_name("address").clear()
         wd.find_element_by_name("address").send_keys("ertwerwte")
+        # home telefon
         wd.find_element_by_name("home").click()
         wd.find_element_by_name("home").clear()
         wd.find_element_by_name("home").send_keys("34534534")
+        # mobile telefon
         wd.find_element_by_name("mobile").click()
         wd.find_element_by_name("mobile").clear()
         wd.find_element_by_name("mobile").send_keys("5687454653456")
+        # email
         wd.find_element_by_name("email").click()
         wd.find_element_by_name("email").clear()
         wd.find_element_by_name("email").send_keys("mous_dom@mail.ru")
+        # b_day
         wd.find_element_by_name("bday").click()
         Select(wd.find_element_by_name("bday")).select_by_visible_text("11")
         wd.find_element_by_name("bday").click()
+        # b_month
         wd.find_element_by_name("bmonth").click()
         Select(wd.find_element_by_name("bmonth")).select_by_visible_text("January")
         wd.find_element_by_name("bmonth").click()
+        # b_year
         wd.find_element_by_name("byear").click()
         wd.find_element_by_name("byear").clear()
         wd.find_element_by_name("byear").send_keys("1974")
+        # submit contact creation
         wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
+        #logout
         wd.find_element_by_link_text("Logout").click()
     
     def is_element_present(self, how, what):
