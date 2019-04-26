e8dbfc12a1fe9b6d6f9774fbefcfdadbec37b977
ezhulinsky <mous_dom@mail.ru>
2019-04-26 18:51:35 +0300

    создан тест в рекордере, перенесен в проект, запуск ок_1(убраны лишние строки/ переименована константа driver/разбито на блоки + коментарии/вспомогательные функции (запуск ок)/вспомогательные функции)

--- Test_Add_Contact.py
+++ Test_Add_Contact.py
@@ -31,89 +31,74 @@ class TestAddContact(unittest.TestCase):
         self.logout(wd)
 
     def logout(self, wd):
-        # logout
         wd.find_element_by_link_text("Logout").click()
 
     def submit_contact_creation(self, wd):
-        # submit contact creation
         wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
 
-    def b_year(self, wd):
-        # b_year
+    def b_year(self, wd, bd_year="1974"):
         wd.find_element_by_name("byear").click()
         wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys("1974")
+        wd.find_element_by_name("byear").send_keys("%s" % bd_year)
 
-    def b_month(self, wd):
-        # b_month
+    def b_month(self, wd, bd_month="January"):
         wd.find_element_by_name("bmonth").click()
-        Select(wd.find_element_by_name("bmonth")).select_by_visible_text("January")
+        Select(wd.find_element_by_name("bmonth")).select_by_visible_text("%s" % bd_month)
         wd.find_element_by_name("bmonth").click()
 
-    def b_day(self, wd):
-        # b_day
+    def b_day(self, wd, bd_day="11"):
         wd.find_element_by_name("bday").click()
-        Select(wd.find_element_by_name("bday")).select_by_visible_text("11")
+        Select(wd.find_element_by_name("bday")).select_by_visible_text("%s" % bd_day)
         wd.find_element_by_name("bday").click()
 
-    def email(self, wd):
-        # email
+    def email(self, wd, email="mous_dom@mail.ru"):
         wd.find_element_by_name("email").click()
         wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys("mous_dom@mail.ru")
+        wd.find_element_by_name("email").send_keys("%s" % email)
 
-    def mobile_telefon(self, wd):
-        # mobile telefon
+    def mobile_telefon(self, wd, mob_telefon="5687454653456"):
         wd.find_element_by_name("mobile").click()
         wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys("5687454653456")
+        wd.find_element_by_name("mobile").send_keys("%s" % mob_telefon)
 
-    def home_telefon(self, wd):
-        # home telefon
+    def home_telefon(self, wd, home_telefon="34534534"):
         wd.find_element_by_name("home").click()
         wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys("34534534")
+        wd.find_element_by_name("home").send_keys("%s" % home_telefon)
 
-    def address(self, wd):
-        # address
+    def address(self, wd, address="ertwerwte"):
         wd.find_element_by_name("address").click()
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys("ertwerwte")
+        wd.find_element_by_name("address").send_keys("%s" % address)
 
-    def lastname(self, wd):
-        # lastname
+    def lastname(self, wd, lastname="kldfg"):
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys("kldfgj")
+        wd.find_element_by_name("lastname").send_keys("%sj" % lastname)
 
-    def middlname(self, wd):
-        # middlname
+    def middlname(self, wd, middlname="gdhf"):
         wd.find_element_by_name("middlename").click()
         wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys("gdhf")
+        wd.find_element_by_name("middlename").send_keys("%s" % middlname)
 
-    def fitstname(self, wd):
-        # fitstname
+    def fitstname(self, wd, fitstname="jdfdsfgs"):
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys("jdfdsfgs")
+        wd.find_element_by_name("firstname").send_keys("%s" % fitstname)
 
     def neu_form(self, wd):
-        # neu form
         wd.find_element_by_link_text("add new").click()
 
-    def login(self, wd):
-        # login
+    def login(self, wd, username="admin", login="secret"):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys("%s" % username)
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys("%s" % login)
         wd.find_element_by_xpath("//input[@value='Login']").click()
 
     def open_home_page(self, wd):
-        # open home page
         wd.get("http://localhost/addressbook/")
 
     def is_element_present(self, how, what):
