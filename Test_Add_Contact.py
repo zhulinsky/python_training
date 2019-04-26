f45cc6d0c1f36272defb462ded7fc3d891380c1c
ezhulinsky <mous_dom@mail.ru>
2019-04-26 18:35:58 +0300

    создан тест в рекордере, перенесен в проект, запуск ок_1(убраны лишние строки/ переименована константа driver/разбито на блоки + коментарии/вспомогательные функции (запуск ок)/)

--- Test_Add_Contact.py
+++ Test_Add_Contact.py
@@ -14,8 +14,95 @@ class TestAddContact(unittest.TestCase):
 
     def test_add_contact(self):
         wd = self.wd
-        # open home page
-        wd.get("http://localhost/addressbook/")
+        self.open_home_page(wd)
+        self.login(wd)
+        self.neu_form(wd)
+        self.fitstname(wd)
+        self.middlname(wd)
+        self.lastname(wd)
+        self.address(wd)
+        self.home_telefon(wd)
+        self.mobile_telefon(wd)
+        self.email(wd)
+        self.b_day(wd)
+        self.b_month(wd)
+        self.b_year(wd)
+        self.submit_contact_creation(wd)
+        self.logout(wd)
+
+    def logout(self, wd):
+        # logout
+        wd.find_element_by_link_text("Logout").click()
+
+    def submit_contact_creation(self, wd):
+        # submit contact creation
+        wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
+
+    def b_year(self, wd):
+        # b_year
+        wd.find_element_by_name("byear").click()
+        wd.find_element_by_name("byear").clear()
+        wd.find_element_by_name("byear").send_keys("1974")
+
+    def b_month(self, wd):
+        # b_month
+        wd.find_element_by_name("bmonth").click()
+        Select(wd.find_element_by_name("bmonth")).select_by_visible_text("January")
+        wd.find_element_by_name("bmonth").click()
+
+    def b_day(self, wd):
+        # b_day
+        wd.find_element_by_name("bday").click()
+        Select(wd.find_element_by_name("bday")).select_by_visible_text("11")
+        wd.find_element_by_name("bday").click()
+
+    def email(self, wd):
+        # email
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys("mous_dom@mail.ru")
+
+    def mobile_telefon(self, wd):
+        # mobile telefon
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys("5687454653456")
+
+    def home_telefon(self, wd):
+        # home telefon
+        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys("34534534")
+
+    def address(self, wd):
+        # address
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys("ertwerwte")
+
+    def lastname(self, wd):
+        # lastname
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys("kldfgj")
+
+    def middlname(self, wd):
+        # middlname
+        wd.find_element_by_name("middlename").click()
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys("gdhf")
+
+    def fitstname(self, wd):
+        # fitstname
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys("jdfdsfgs")
+
+    def neu_form(self, wd):
+        # neu form
+        wd.find_element_by_link_text("add new").click()
+
+    def login(self, wd):
         # login
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
@@ -24,53 +111,11 @@ class TestAddContact(unittest.TestCase):
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys("secret")
         wd.find_element_by_xpath("//input[@value='Login']").click()
-        # neu form
-        wd.find_element_by_link_text("add new").click()
-        # fitstname
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys("jdfdsfgs")
-        # middlname
-        wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys("gdhf")
-        # lastname
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys("kldfgj")
-        # address
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys("ertwerwte")
-        # home telefon
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys("34534534")
-        # mobile telefon
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys("5687454653456")
-        # email
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys("mous_dom@mail.ru")
-        # b_day
-        wd.find_element_by_name("bday").click()
-        Select(wd.find_element_by_name("bday")).select_by_visible_text("11")
-        wd.find_element_by_name("bday").click()
-        # b_month
-        wd.find_element_by_name("bmonth").click()
-        Select(wd.find_element_by_name("bmonth")).select_by_visible_text("January")
-        wd.find_element_by_name("bmonth").click()
-        # b_year
-        wd.find_element_by_name("byear").click()
-        wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys("1974")
-        # submit contact creation
-        wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
-        #logout
-        wd.find_element_by_link_text("Logout").click()
-    
+
+    def open_home_page(self, wd):
+        # open home page
+        wd.get("http://localhost/addressbook/")
+
     def is_element_present(self, how, what):
         try: self.wd.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
