401c9c494133e5682f4462aa0c281cbed6294d53
ezhulinsky <mous_dom@mail.ru>
2019-04-26 17:09:35 +0300

    создан тест в рекордере, перенесен в проект, запуск ок_1(убраны лишние строки)

--- Test_Add_Contact.py
+++ Test_Add_Contact.py
@@ -11,10 +11,7 @@ class TestAddContact(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox()
         self.driver.implicitly_wait(30)
-        self.base_url = "https://www.katalon.com/"
-        self.verificationErrors = []
-        self.accept_next_alert = True
-    
+
     def test_add_contact(self):
         driver = self.driver
         driver.get("http://localhost/addressbook/")
@@ -68,21 +65,9 @@ class TestAddContact(unittest.TestCase):
         try: self.driver.switch_to_alert()
         except NoAlertPresentException as e: return False
         return True
-    
-    def close_alert_and_get_its_text(self):
-        try:
-            alert = self.driver.switch_to_alert()
-            alert_text = alert.text
-            if self.accept_next_alert:
-                alert.accept()
-            else:
-                alert.dismiss()
-            return alert_text
-        finally: self.accept_next_alert = True
-    
+
     def tearDown(self):
         self.driver.quit()
-        self.assertEqual([], self.verificationErrors)
 
 if __name__ == "__main__":
     unittest.main()
