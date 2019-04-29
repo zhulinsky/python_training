b5e9008f738ce6183a5f15b8293864706c4f8d0a
ezhulinsky <mous_dom@mail.ru>
2019-04-29 16:14:17 +0300

    ПЕРЕДЕЛКА/тест заново перенесен в проект,(убраны лишние строки/переименована константа driver/коментарии/вспомогательные методы/выполнена параметризация методов/введены вспомогательные классв(запуск ок))

--- Test_Add_Contact.py
+++ Test_Add_Contact.py
@@ -6,6 +6,11 @@ from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest, time, re
+from group_contact import Group_l_in
+from group_contact import Group_name
+from group_contact import Group_tel
+from group_contact import Group_bd
+
 
 class TestAddContact(unittest.TestCase):
     def setUp(self):
@@ -15,13 +20,13 @@ class TestAddContact(unittest.TestCase):
     def test_add_contact(self):
         wd = self.wd
         self.open_home_page(wd)
-        self.login(wd, "admin", "secret")
+        self.login(wd, Group_l_in (login = "admin", password = "secret"))
         self.open_neu_page(wd)
-        self.name(wd, "jdfdsfgs", "gdhf", "kldfgj")
+        self.name(wd, Group_name (firstname = "evgen", middlename = "lavrov", lastname = "ivanovich"))
         self.address(wd, "ertwerwte")
-        self.telefon(wd, "34534534", "5687454653456")
+        self.telefon(wd, Group_tel (home_tel = "34534534", mobile_tel = "5687454653456"))
         self.email(wd, "mous_dom@mail.ru")
-        self.bd(wd, "1974", "January", "11")
+        self.bd(wd, Group_bd (bday = "11", bmonth =  "January", byear ="1974"))
         self.sabmit(wd)
         self.logout(wd)
 
@@ -32,56 +37,56 @@ class TestAddContact(unittest.TestCase):
     def sabmit(self, wd):
         wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
 
-    def bd(self, wd, byear, bmounth, bday):
+    def bd(self, wd, group_contact):
         wd.find_element_by_name("bday").click()
-        Select(wd.find_element_by_name("bday")).select_by_visible_text("%s" % bday)
+        Select(wd.find_element_by_name("bday")).select_by_visible_text("%s" % group_contact.bday)
         wd.find_element_by_name("bday").click()
         wd.find_element_by_name("bmonth").click()
-        Select(wd.find_element_by_name("bmonth")).select_by_visible_text("%s" % bmounth)
+        Select(wd.find_element_by_name("bmonth")).select_by_visible_text("%s" % group_contact.bmonth)
         wd.find_element_by_name("bmonth").click()
         wd.find_element_by_name("byear").click()
         wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys("%s" % byear)
+        wd.find_element_by_name("byear").send_keys("%s" % group_contact.byear)
 
     def email(self, wd, email):
         wd.find_element_by_name("email").click()
         wd.find_element_by_name("email").clear()
         wd.find_element_by_name("email").send_keys("%s" % email)
-
-    def telefon(self, wd, home_tel, mobile_tel):
+#
+    def telefon(self, wd, group_contact):
         wd.find_element_by_name("home").click()
         wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys("%s" % home_tel)
+        wd.find_element_by_name("home").send_keys("%s" % group_contact.home_tel)
         wd.find_element_by_name("mobile").click()
         wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys("%s" % mobile_tel)
+        wd.find_element_by_name("mobile").send_keys("%s" % group_contact.mobile_tel)
 
     def address(self, wd, address):
         wd.find_element_by_name("address").click()
         wd.find_element_by_name("address").clear()
         wd.find_element_by_name("address").send_keys("%s" % address)
-
-    def name(self, wd, firstname, middlname, lastname):
+#
+    def name(self, wd, group_contact):
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys("%s" % firstname)
+        wd.find_element_by_name("firstname").send_keys("%s" % group_contact.firstname)
         wd.find_element_by_name("middlename").click()
         wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys("%s" % middlname)
+        wd.find_element_by_name("middlename").send_keys("%s" % group_contact.middlename)
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys("%s" % lastname)
+        wd.find_element_by_name("lastname").send_keys("%s" % group_contact.lastname)
 
     def open_neu_page(self, wd):
         wd.find_element_by_link_text("add new").click()
-
-    def login(self, wd, user_name, password):
+#
+    def login(self, wd, group_contact):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("%s" % user_name)
+        wd.find_element_by_name("user").send_keys("%s" % group_contact.login)
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("%s" % password)
+        wd.find_element_by_name("pass").send_keys("%s" % group_contact.password)
         wd.find_element_by_xpath("//input[@value='Login']").click()
 
     def open_home_page(self, wd):
