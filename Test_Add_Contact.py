dec4c1d9bc13165fca9c81e9f65abd10670b1ea7
ezhulinsky <mous_dom@mail.ru>
2019-04-29 13:35:18 +0300

    ПЕРЕДЕЛКА/тест заново перенесен в проект,(убраны лишние строки/переименована константа driver/коментарии/вспомогательные методы(запуск ок))

--- Test_Add_Contact.py
+++ Test_Add_Contact.py
@@ -14,44 +14,25 @@ class TestAddContact(unittest.TestCase):
 
     def test_add_contact(self):
         wd = self.wd
-        # open home page
-        wd.get("http://localhost/addressbook/")
-        #login
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-        # open neu page
-        wd.find_element_by_link_text("add new").click()
-        # name
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys("jdfdsfgs")
-        wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys("gdhf")
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys("kldfgj")
-        # address
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys("ertwerwte")
-        #telefon
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys("34534534")
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys("5687454653456")
-        # email
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys("mous_dom@mail.ru")
-        # bd
+        self.open_home_page(wd)
+        self.login(wd)
+        self.open_neu_page(wd)
+        self.name(wd)
+        self.address(wd)
+        self.telefon(wd)
+        self.email(wd)
+        self.bd(wd)
+        self.sabmit(wd)
+        self.logout(wd)
+
+    def logout(self, wd):
+        # logout
+        wd.find_element_by_link_text("Logout").click()
+
+    def sabmit(self, wd):
+        wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
+
+    def bd(self, wd):
         wd.find_element_by_name("bday").click()
         Select(wd.find_element_by_name("bday")).select_by_visible_text("11")
         wd.find_element_by_name("bday").click()
@@ -61,11 +42,51 @@ class TestAddContact(unittest.TestCase):
         wd.find_element_by_name("byear").click()
         wd.find_element_by_name("byear").clear()
         wd.find_element_by_name("byear").send_keys("1974")
-        #sabmit
-        wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
-        #logout
-        wd.find_element_by_link_text("Logout").click()
-    
+
+    def email(self, wd):
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys("mous_dom@mail.ru")
+
+    def telefon(self, wd):
+        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys("34534534")
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys("5687454653456")
+
+    def address(self, wd):
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys("ertwerwte")
+
+    def name(self, wd):
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys("jdfdsfgs")
+        wd.find_element_by_name("middlename").click()
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys("gdhf")
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys("kldfgj")
+
+    def open_neu_page(self, wd):
+        wd.find_element_by_link_text("add new").click()
+
+    def login(self, wd):
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def open_home_page(self, wd):
+        wd.get("http://localhost/addressbook/")
+
     def is_element_present(self, how, what):
         try: self.wd.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
